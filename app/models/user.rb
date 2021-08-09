64ffe5b8d98444377990df6b4467aa0e30953c43
Sebastian <sebastianwebp@gmail.com>
2021-08-09 19:30:15 -0400

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
                     uniqueness: { case_sensitive: false },  
                      length: {  maximum:105},
                      format: {with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
