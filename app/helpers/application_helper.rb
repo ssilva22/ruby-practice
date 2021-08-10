f33ccc0a92cd17f75012680b554d85f595dec003
Sebastian <sebastianwebp@gmail.com>
2021-08-10 12:55:54 -0400

    improved layout of the yser section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-    def gravatar_for(user , options = { size:200 })
+    def gravatar_for(user , options = { size:80 })
         email_address = user.email.downcase
         hash = Digest::MD5.hexdigest(email_address)
         size = options[:size]
