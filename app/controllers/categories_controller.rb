d836da313fa490bfe9e53e66c315e68ffab03c75
Sebastian <sebastianwebp@gmail.com>
2021-08-11 14:05:01 -0400

    ready for deployment

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
+    ARTICLES_PER_PAGE= 5
 
     def new
         @category = Category.new
@@ -14,18 +16,40 @@ class CategoriesController < ApplicationController
     end
 end
 
-    def index
+def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
   
+
+    def index
+        @page =params.fetch(:page , 0).to_i
+        @categories = Category.offset(@page*ARTICLES_PER_PAGE).limit(ARTICLES_PER_PAGE)
     end
   
     def show
         @category = Category.find(params[:id])
     end
 
-
     private
 
     def category_params
       params.require(:category).permit(:name)
     end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+          flash[:alert] = "Only admins can perform that action"
+          redirect_to categories_path
+        end
+      end
   end 
