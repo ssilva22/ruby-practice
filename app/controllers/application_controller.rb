9131e76cec8ab3dd289a638fba043f7076a134e4
Sebastian <sebastianwebp@gmail.com>
2021-08-11 12:52:20 -0400

    fixed bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
     def require_user
         if !logged_in?
          flash[:alert] = "You must be logged in to perform that action"
+        end
     end
 end
